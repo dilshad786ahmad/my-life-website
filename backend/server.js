ec71aeeb17f8c171e285995fb251a3db2bbeab64
dilshad ahmad <da9574091@gmail.com>
2026-05-12 13:04:26 +0530

    Fix CORS and PORT for production

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 // ✅ Middlewares
 // app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:5173', // Aapke frontend ka exact URL
+    origin: ['http://localhost:5173', 'https://my-life-website-cjdr.vercel.app'], // Aapke frontend ka exact URL
     credentials: true,               // Cookies allow karne ke liye zaroori hai
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
@@ -91,7 +91,7 @@ app.use("/api/feedback", clientFeedbackRoutes);
 
 
 // Port
-const PORT =5000 ;
+const PORT = process.env.PORT || 5000;
 
 // ✅ Better server start (DB ke baad)
 const startServer = async () => {
