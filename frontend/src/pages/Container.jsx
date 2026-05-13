9a71677b743456f4014e440550a8650be2c9b3b6
dilshad ahmad <da9574091@gmail.com>
2026-05-13 07:30:14 +0530

    feat: Implement scroll-based lazy loading for homepage sections

--- frontend/src/pages/Container.jsx
+++ frontend/src/pages/Container.jsx
@@ -5,16 +5,23 @@ import About from './About'
 import Team from "./our_team"
 import WhatIOffer from './Offer'
 import SpecializedSolutions from './Contact'
+import LazySection from '../components/LazySection'
 
 const Container = () => {
   return (
    <>
    <Home/>
    {/* <ExcellenceSection/> */}
-   <About/>
+   <LazySection minHeight="80vh">
+     <About/>
+   </LazySection>
    {/* <Team/> */}
-   <WhatIOffer/>
-   <SpecializedSolutions/>
+   <LazySection minHeight="80vh">
+     <WhatIOffer/>
+   </LazySection>
+   <LazySection minHeight="80vh">
+     <SpecializedSolutions/>
+   </LazySection>
    </>
   )
 }
